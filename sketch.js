d980a30ac54541d9527eccbb5b809e5c24af30a6
technoblade3 <101042547+technoblade3@users.noreply.github.com>
2022-04-30 12:41:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,15 +35,8 @@ function draw() {
   rabbit.collide(edges);
   
    drawSprites();
-   
-  
-// var select_sprites = Math(random(1,3));
 
-// var select_sprites = Math.random(random(1,3));
-
-// var select_sprites = Math.round(1,3);
-
-// var select_sprites = Math.round(random(1,3));
+ var select_sprites = Math.round(random(1,3));
 
   
   // if (frameCount % 50 == 0) {
@@ -74,15 +67,15 @@ function draw() {
   //   }
   // }
 
-  // if (frameCount % 80 = 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
-  // }
+   if (frameCount % 80 == 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }else {
+       createRed();
+     }
+   }
 
 
 
